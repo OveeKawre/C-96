5c9a46bedb61c3b330557e16379f3f2817ffe13f
OveeKawre <74299130+OveeKawre@users.noreply.github.com>
2023-06-05 10:39:06 +0530

    Add files via upload

---
+++ Feed.js
@@ -0,0 +1,204 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import StoryCard from "./StoryCard";
+
+import * as Font from "expo-font";
+import { FlatList } from "react-native-gesture-handler";
+import firebase from "firebase";
+
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+let recipes = require("./temp_recipes.json");
+
+export default class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      light_theme: true,
+      stories: []
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchRecipes();
+    this.fetchUser();
+  }
+
+  fetchRecipes = () => {
+    firebase
+      .database()
+      .ref("/posts/")
+      .on(
+        "value",
+        snapshot => {
+          let recipes = [];
+          if (snapshot.val()) {
+            Object.keys(snapshot.val()).forEach(function (key) {
+              recipes.push({
+                key: key,
+                value: snapshot.val()[key]
+              });
+            });
+          }
+          this.setState({ recipes: recipes });
+          this.props.setUpdateToFalse();
+        },
+        function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        }
+      );
+  };
+
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+
+  renderItem = ({ item: recipe }) => {
+    return <StoryCard recipe={recipe} navigation={this.props.navigation} />;
+  };
+
+  keyExtractor = (item, index) => index.toString();
+
+  render() {
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      return (
+        <View
+          style={
+            this.state.light_theme ? styles.containerLight : styles.container
+          }
+        >
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text
+                style={
+                  this.state.light_theme
+                    ? styles.appTitleTextLight
+                    : styles.appTitleText
+                }
+              >
+                Cook & Bake
+              </Text>
+            </View>
+          </View>
+          {!this.state.recipes[0] ? (
+            <View style={styles.noRecipes}>
+              <Text
+                style={
+                  this.state.light_theme
+                    ? styles.noRecipesTextLight
+                    : styles.noRecipesText
+                }
+              >
+                No Recipes Available
+              </Text>
+            </View>
+          ) : (
+            <View style={styles.cardContainer}>
+              <FlatList
+                keyExtractor={this.keyExtractor}
+                data={this.state.recipes}
+                renderItem={this.renderItem}
+              />
+            </View>
+          )}
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c"
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  appTitleTextLight: {
+    color: "black",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  cardContainer: {
+    flex: 0.85
+  },
+  noRecipes: {
+    flex: 0.85,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  noRecipesTextLight: {
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  },
+  noRecipesText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans"
+  }
+});
---
+++ StoryCard.js
@@ -0,0 +1,283 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Dimensions,
+  TouchableOpacity
+} from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { RFValue } from "react-native-responsive-fontsize";
+import * as Font from "expo-font";
+import firebase from "firebase";
+
+import * as SplashScreen from 'expo-splash-screen';
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
+
+export default class StoryCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      light_theme: true,
+      recipe_id: this.props.recipe.key,
+      recipe_data: this.props.recipe.value,
+      is_liked: false,
+      likes: this.props.story.value.likes
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  likeAction = () => {
+    if (this.state.is_liked) {
+      firebase
+        .database()
+        .ref("posts")
+        .child(this.state.recipe_id)
+        .child("likes")
+        .set(firebase.database.ServerValue.increment(-1));
+      this.setState({ likes: (this.state.likes -= 1), is_liked: false });
+    } else {
+      firebase
+        .database()
+        .ref("posts")
+        .child(this.state.recipe_id)
+        .child("likes")
+        .set(firebase.database.ServerValue.increment(1));
+      this.setState({ likes: (this.state.likes += 1), is_liked: true });
+    }
+  };
+  fetchUser = () => {
+    let theme;
+    firebase
+      .database()
+      .ref("/users/" + firebase.auth().currentUser.uid)
+      .on("value", snapshot => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === "light" });
+      });
+  };
+
+  render() {
+    let recipe = this.state.recipe_data;
+    if (this.state.fontsLoaded) {
+      SplashScreen.hideAsync();
+      let images = {
+        image_1: require("../assets/story_image_1.png"),
+        image_2: require("../assets/story_image_2.png"),
+        image_3: require("../assets/story_image_3.png"),
+        image_4: require("../assets/story_image_4.png"),
+        image_5: require("../assets/story_image_5.png")
+      };
+      return (
+        <TouchableOpacity
+          style={styles.container}
+          onPress={() =>
+            this.props.navigation.navigate("RecipeScreen", {
+              recipe: this.props.recipe
+            })
+          }
+        >
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View
+            style={
+              this.state.light_theme
+                ? styles.cardContainerLight
+                : styles.cardContainer
+            }
+          >
+            <Image
+              source={images[recipe.preview_image]}
+              style={styles.recipeImage}
+            ></Image>
+            <View style={styles.titleContainer}>
+              <View style={styles.titleTextContainer}>
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.recipeTitleTextLight
+                      : styles.recipeTitleText
+                  }
+                >
+                  {recipe.title}
+                </Text>
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.recipeAuthorTextLight
+                      : styles.recipeAuthorText
+                  }
+                >
+                  {recipe.author}
+                </Text>
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.descriptionTextLight
+                      : styles.descriptionText
+                  }
+                >
+                  {this.props.recipe.description}
+                </Text>
+              </View>
+            </View>
+
+            <View style={styles.actionContainer}>
+              <TouchableOpacity
+                style={
+                  this.state.is_liked
+                    ? styles.likeButtonLiked
+                    : styles.likeButtonDisliked
+                }
+                onPress={() => this.likeAction()}
+              >
+                <Ionicons
+                  name={"heart"}
+                  size={RFValue(30)}
+                  color={this.state.light_theme ? "black" : "white"}
+                />
+
+                <Text
+                  style={
+                    this.state.light_theme
+                      ? styles.likeTextLight
+                      : styles.likeText
+                  }
+                >
+                  {this.state.likes}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </TouchableOpacity>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  cardContainer: {
+    margin: RFValue(13),
+    backgroundColor: "#2f345d",
+    borderRadius: RFValue(20)
+  },
+  cardContainerLight: {
+    margin: RFValue(13),
+    backgroundColor: "white",
+    borderRadius: RFValue(20),
+    shadowColor: "rgb(0, 0, 0)",
+    shadowOffset: {
+      width: 3,
+      height: 3
+    },
+    shadowOpacity: RFValue(0.5),
+    shadowRadius: RFValue(5),
+    elevation: RFValue(2)
+  },
+  recipeImage: {
+    resizeMode: "contain",
+    width: "95%",
+    alignSelf: "center",
+    height: RFValue(250)
+  },
+  titleContainer: {
+    paddingLeft: RFValue(20),
+    justifyContent: "center"
+  },
+  titleTextContainer: {
+    flex: 0.8
+  },
+  iconContainer: {
+    flex: 0.2
+  },
+  recipeTitleText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    color: "white"
+  },
+  recipeTitleTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(25),
+    color: "black"
+  },
+  recipeAuthorText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(18),
+    color: "white"
+  },
+  recipeAuthorTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(18),
+    color: "black"
+  },
+  descriptionContainer: {
+    marginTop: RFValue(5)
+  },
+  descriptionText: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(13),
+    color: "white"
+  },
+  descriptionTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: RFValue(13),
+    color: "black"
+  },
+  actionContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: RFValue(10)
+  },
+  likeButtonLiked: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    backgroundColor: "#eb3948",
+    borderRadius: RFValue(30)
+  },
+  likeButtonDisliked: {
+    width: RFValue(160),
+    height: RFValue(40),
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row",
+    borderColor: "#eb3948",
+    borderWidth: 2,
+    borderRadius: RFValue(30)
+  },
+  likeText: {
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 25,
+    marginLeft: 25,
+    marginTop: 6
+  },
+  likeTextLight: {
+    fontFamily: "Bubblegum-Sans",
+    fontSize: 25,
+    marginLeft: 25,
+    marginTop: 6
+  }
+});
